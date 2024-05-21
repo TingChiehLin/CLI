1bbf9dbabd21848e5046e2735c38810b209da10c
TingChiehLin <cooloojayoo@gmail.com>
2024-05-21 20:28:58 +1000

    Add - Book model & table

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import (
     ForeignKey,
     Column,
@@ -46,21 +48,21 @@ class User(Base):
         return f"User {self.id}: " + f"Name {self.name}, " + f"Email: {self.email}"
 
 
-# class Booking(Base):
-#     __tablename__ = "bookings"
+class Booking(Base):
+    __tablename__ = "bookings"
 
-#     id = Column(Integer, primary_key=True)
-#     time = Column(DateTime(), default=datetime.now())
-#     date = Column(String)
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime(), default=datetime.now())
+    date = Column(String)
 
-#     created_at = Column(DateTime(), server_default=func.now())
-#     updated_at = Column(DateTime(), onupdate=func.now())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
 
-#     user_id = Column(Integer(), ForeignKey("users.id"))
-#     restaurant_id = Column(Integer(), ForeignKey("restaurant.id"))
+    # user_id = Column(Integer(), ForeignKey("users.id"))
+    # restaurant_id = Column(Integer(), ForeignKey("restaurant.id"))
 
-#     def __repr__(self):
-#         return f"Booking: {self.id} " + f"Time: {self.time}" + f"Date: {self.date}"
+    def __repr__(self):
+        return f"Booking: {self.id} " + f"Time: {self.time}" + f"Date: {self.date}"
 
 
 # many to many
@@ -72,19 +74,19 @@ class User(Base):
 # a restaurant can have many bookings
 
 
-# class Restaurant(Base):
-#     __tablename__ = "restaurants"
+class Restaurant(Base):
+    __tablename__ = "restaurants"
 
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String)
-#     rate = Column(Integer())
+    id = Column(Integer(), primary_key=True)
+    name = Column(String)
+    rate = Column(Integer())
 
-#     # bookings = relationship("Booking", backref=backref("restaurant"))
-#     # users = relationship(
-#     #     "User", secondary=restaurant_user, back_populates="restaurants"
-#     # )
+    # bookings = relationship("Booking", backref=backref("restaurant"))
+    # users = relationship(
+    #     "User", secondary=restaurant_user, back_populates="restaurants"
+    # )
 
-#     def __repr__(self):
-#         return (
-#             f"Restaurant(id:{self.id},)" + f"Name: {self.name}" + f"rate:{self.rate},"
-#         )
+    def __repr__(self):
+        return (
+            f"Restaurant(id:{self.id},)" + f"Name: {self.name}" + f"rate:{self.rate},"
+        )
